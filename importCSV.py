0c87076bbc50845938b432839b7656182bda7694
Alex <alex@SENECAOH.local>
2019-12-02 15:15:24 -0500

    import recommended, change index to show recommended and renewal acres

--- importCSV.py
+++ importCSV.py
@@ -41,21 +41,7 @@ cauv_dict = {
 
 for row in open('2019RENEWAL_CAUV_APPS_DATABASE.csv'):
     line = row.split(',')
-    cauv_dict['AG_APP'] = line[0]
-    cauv_dict['Parcels_Combined_Acres'] = line[1]
     cauv_dict['Parcel_Change_Check'] = line[2]
-    cauv_dict['Commodity_Acres'] = line[3]
-    cauv_dict['Hay_Acres'] = line[4]
-    cauv_dict['Perm_Pasture_Acres'] = line[5]
-    cauv_dict['Noncommercial_Wood_Acres'] = line[6]
-    cauv_dict['Commerical_Wood_Acres'] = line[7]
-    cauv_dict['Other_Crop_Acres'] = line[8]
-    cauv_dict['Homesite_Acres'] = line[9]
-    cauv_dict['Road_Waste_Pond_Acres'] = line[10]
-    cauv_dict['CRP_Acres'] = line[11]
-    cauv_dict['Con25_Acres'] = line[12]
-    cauv_dict['Other_Use_Acres'] = line[13]
-    cauv_dict['Stated_Total_Acres'] = line[14]
     cauv_dict['Farmed_Acres_1'] = line[15]
     cauv_dict['Use_of_Land_1'] = line[16]
     cauv_dict['Units_Acre_1'] = line[17]
@@ -71,6 +57,24 @@ for row in open('2019RENEWAL_CAUV_APPS_DATABASE.csv'):
     cauv_dict['Units_Acre_3'] = line[27]
     cauv_dict['Price_Unit_3'] = line[28]
     cauv_dict['Gross_Income_3'] = line[29].strip()
+
+for row in open('11_22_2019_RECOMMENDED.csv'):
+    line = row.split(',')
+    cauv_dict['AG_APP'] = line[0]
+    cauv_dict['Parcels_Combined_Acres'] = line[1]
+    cauv_dict['Stated_Total_Acres'] = line[2]
+    cauv_dict['Commodity_Acres'] = line[3]
+    cauv_dict['Hay_Acres'] = line[4]
+    cauv_dict['Perm_Pasture_Acres'] = line[5]
+    cauv_dict['Noncommercial_Wood_Acres'] = line[6]
+    cauv_dict['Commerical_Wood_Acres'] = line[7]
+    cauv_dict['Other_Crop_Acres'] = line[8]
+    cauv_dict['Homesite_Acres'] = line[9]
+    cauv_dict['Road_Waste_Pond_Acres'] = line[10]
+    cauv_dict['CRP_Acres'] = line[11]
+    cauv_dict['Con25_Acres'] = line[12]
+    cauv_dict['Other_Use_Acres'] = line[13]
+
     for each in cauv_dict:
         app = PreviousCAUVApp(
             user="BC",
