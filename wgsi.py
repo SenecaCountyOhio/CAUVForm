57305c714daf8fc8371bcc143d2108e40fad8d25
Alex <alex@SENECAOH.local>
2019-12-12 11:12:18 -0500

    set threaded=True

--- wgsi.py
+++ wgsi.py
@@ -5,6 +5,7 @@ app = create_app()
 if __name__ == "__main__":
     app.run(
         host='192.168.26.116',
-        debug=True,
+        debug=False,
         port=5000,
+        threaded=True,
     )
